983626d7f7d9935b90643dd6ad44c60a8da40e02
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-09 22:34:15 +0545

    created signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import GlobalStyle from "./globalStyles";
 import Home from "./pages/HomePage/Home";
 import Services from "./pages/Services/Services";
 import Products from "./pages/Products/Products";
+import SignUp from "./pages/SignUp/SignUp";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
         <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
     </Router>
   );
