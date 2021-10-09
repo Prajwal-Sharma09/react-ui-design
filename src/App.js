3ce9eae19e0d9319fd0d3f930a0ed1b595248e30
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-10-09 22:29:17 +0545

    created products page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyle from "./globalStyles";
 import Home from "./pages/HomePage/Home";
 import Services from "./pages/Services/Services";
+import Products from "./pages/Products/Products";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components";
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
       </Switch>
     </Router>
   );
